18510bd076adf5c9f8c806acc052271534547070
Kevin Rodriguez <kevin94rdgz@gmail.com>
2016-05-02 16:41:32 -0400

    first commit

---
+++ src/build-a-personal-portfolio-webpage/index.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+  http = require('http'),
+  path = require('path'),
+  reload = require('reload'),
+  bodyParser = require('body-parser'),
+  logger = require('morgan');
+ 
+var app = express();
+ 
+var publicDir = path.join(__dirname, '/public');
+ 
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+//parses json, multi-part (file), url-encoded
+app.use(bodyParser.json());
+//Serve static files
+app.use('/public', express.static(publicDir));
+ 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(publicDir, 'index.html'));
+})
+ 
+var server = http.createServer(app);
+ 
+//reload code here 
+//optional reload delay and wait argument can be given to reload, refer to [API](https://github.com/jprichardson/reload#api) below 
+reload(server, app);
+ 
+server.listen(app.get('port'), function(){
+  console.log("Web server listening on port " + app.get('port'));
+});
---
+++ src/build-a-personal-portfolio-webpage/public/js/writer.js
@@ -0,0 +1,23 @@
+
+(function writer() {
+  var elem = document.querySelector('#writer');
+  if (!elem) return;
+  var pause = 800;
+  var opts = {
+    typeSpeed: 50,
+    deleteSpeed: 50,
+    loop: false,
+    postfix: ''
+  }
+
+  malarkey(elem, opts)
+  .pause(1800).delete()
+  .type('IT Professional').pause(pause).delete()
+  .type('CompTIA A+ Certified').pause(pause).delete()
+  .type('CompTIA Network+ Certified').pause(pause).delete()
+  .type('Full-Stack Web Developer').pause(pause).delete()
+  .type('Kevin Rodriguez.')
+
+}())
+
+
